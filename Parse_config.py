c616d21f2a4acbf3e0d69c691a61d9d0f55c6f74
BraniH <wbranislavhrasko@gmail.com>
2023-08-01 21:40:53 +0200

    base design without tunnels handeled.

--- Parse_config.py
+++ Parse_config.py
@@ -8,12 +8,28 @@ def get_txt_content(path, encoding='utf-8'):
 def write_file(path, content, encoding='utf-8'):
     with open(path, 'w', encoding=encoding) as file:
         file.writelines(content)
+        
+def cidr_to_subnet_mask(cidr):
+    subnet_mask = [0, 0, 0, 0]
+    
+    # Calculate the number of bits for the network and host portions
+    num_network_bits = cidr
+    num_host_bits = 32 - cidr
+    
+    # Set the network bits to 1 in the subnet_mask
+    for i in range(num_network_bits):
+        subnet_mask[i // 8] |= (1 << (7 - i % 8))
+    
+    # Convert each octet of subnet_mask to decimal representation
+    subnet_mask_decimals = ".".join(str(octet) for octet in subnet_mask)
+    
+    return subnet_mask_decimals
+
 
 class ParseConfig:
     @staticmethod
     def _replacement_rules(line, setup_config):
         condition_line = line.lower()
-        
         if "<xxxnr0000aaaa101>" in condition_line or "<hostname>" in condition_line:
             line = condition_line.replace("<xxxnr0000aaaa101>", setup_config["WAN info"]["Hostname"]) \
                                  .replace("<hostname>", setup_config["WAN info"]["Hostname"])
@@ -29,7 +45,21 @@ class ParseConfig:
             line = condition_line.replace("<country", setup_config["WAN info"]["Hostname"][:3]) \
                                  .replace("city", setup_config["Location info"]["City"]) \
                                  .replace("sidxxxx>", f"SID{setup_config['WAN info']['Hostname'][5:9]}")
+        elif "<wan interface 1>" in condition_line:
+            line = condition_line.replace("<wan interface 1>", "g0/0/1")
+        elif "<public ip> | dhcp" in condition_line:
+            if str(setup_config["Main Link"]["Main_IP+mask"]).lower() != "dhcp":
+                ip, mask = setup_config["Main Link"]["Main_IP+mask"].split("/")
+                line = condition_line.replace("<public ip> | dhcp", ip + " " + cidr_to_subnet_mask(int(mask)))
+            else:
+                line = condition_line.replace("<public ip> | dhcp", "dhcp")
+        elif "<gw>" in condition_line and setup_config["WAN info"]["Design"].upper() == "BASE":
+            if str(setup_config["Main Link"]["Main_IP+mask"]).lower() != "dhcp":
+                 line = condition_line.replace("<gw>", setup_config["Main Link"]["Main_GW"])
+            else:
+                line = condition_line.replace("ip route vrf inet 0.0.0.0 0.0.0.0 <gw>", "")
         
+
         return line
     
     
@@ -42,7 +72,7 @@ class ParseConfig:
         self.path_to_config = path_to_config
         self.setup_config = setup_config
         config = get_txt_content(self.path_to_config)
-        # print(final_config)
+        print(self.setup_config)
         # print(self.path_to_config)
         final_config = ParseConfig.look_and_replace_strings(config, setup_config)
         write_file(self.path_to_config, final_config)
